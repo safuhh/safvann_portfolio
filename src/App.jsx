b0919085cba507d145e40913928702623c6f65b5
Rihen <rihenww@gmail.com>
2026-06-05 12:30:24 +0530

    cursor splash

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,24 @@ import ExperienceSection from "./components/Experience";
 import ProjectsSection from "./components/Project";
 import ContactSection from "./components/Contact";
 import Footer from "./components/Footer";
+import SplashCursor from "./components/SplashCursor";
 
 function App() {
   return (
     <div className="bg-black text-white">
       <LenisSmoothScroll>
+        <SplashCursor
+          DENSITY_DISSIPATION={3.5}
+          VELOCITY_DISSIPATION={2}
+          PRESSURE={0.1}
+          CURL={3}
+          SPLAT_RADIUS={0.2}
+          SPLAT_FORCE={6000}
+          COLOR_UPDATE_SPEED={10}
+          SHADING
+          RAINBOW_MODE={false}
+          COLOR="#A855F7"
+        />
         <Navbar />
         <HomePage />
         <AboutSection />
